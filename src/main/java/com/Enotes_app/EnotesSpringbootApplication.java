d1824bdeaf3c41cee086080a0ad76a79834d9f4b
Chandan <kchandanbaral29@gmail.com>
2024-12-02 09:57:45 -0800

    initial commit

---
+++ src/main/java/com/Enotes_app/EnotesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.Enotes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesSpringbootApplication.class, args);
+	}
+
+}
